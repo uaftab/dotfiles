4916e2f156f87c53a1ec3b282b8a021295bbba6d
lemniscate <umair.ops@gmail.com>
2016-02-20 11:43:53 -0800

    Battery Plugin

---
+++ battery_install.sh
@@ -0,0 +1,7 @@
+sudo curl -O https://raw.githubusercontent.com/holman/spark/master/spark
+sudo mv spark /usr/local/bin
+sudo chmod u+x /usr/local/bin/spark
+
+sudo curl -O https://raw.githubusercontent.com/goles/battery/master/battery
+sudo mv battery /usr/local/bin
+sudo chmod u+x /usr/local/bin/battery
