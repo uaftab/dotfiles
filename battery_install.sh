5b74115167c53fb95da96800af6558f43bbbe3a0
lemniscate snickets <lemniscate@localhost.localdomain>
2016-10-16 14:45:15 -0400

    Added installers for themes

--- battery_install.sh
+++ battery_install.sh
@@ -5,3 +5,6 @@ sudo chmod u+x /usr/local/bin/spark
 sudo curl -O https://raw.githubusercontent.com/goles/battery/master/battery
 sudo mv battery /usr/local/bin
 sudo chmod u+x /usr/local/bin/battery
+
+sudo sh -c "curl https://raw.githubusercontent.com/holman/spark/master/spark -o /usr/local/bin/spark && chmod +x /usr/local/bin/spark"
+
---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#Author: Lemniscate Snickets  
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+sudo apt-get install -y linux-headers-$(uname -r)
+
+sudo apt-get install -y \
+	vim terminator \
+	build-essential clang llvm autoconf \
+	cmake git ninja-build \
+	wget curl \
+	python python-dev \
+	
+
+#install rust
+curl -sSf https://static.rust-lang.org/rustup.sh | sh
+
+
+#install vundle 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .vim* ~/
+vim +PluginInstall +qall
+
+cp -R terminator/ ~/config/
+sudo apt-get install -y xfce4-*
+sudo apt-get install -y xfce-keyboard-shortcuts
+sudo apt-get install cabextract font-manager
+sudo apt-get install gtk2-engines-murrine
+cd ~
+mkdir -p ~/.themes
+cd .themes 
+wget https://github.com/shimmerproject/Greybird/archive/master.zip
+unzip master.zip
+rm master.zip
+cd ~
+mkdir -p ~/.icons
+cd .icons 
+wget https://github.com/shimmerproject/elementary-xfce/archive/master.zip
+unzip master.zip
+mv elementary*/* .
+rm master.zip
+
+sudo ./battery_install.sh
