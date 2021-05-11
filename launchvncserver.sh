ec5ab0405fd2b61e6b4adb3f1e82d7d2e96d98fb
lemniscate <contact@uaftab.com>
2021-05-10 21:44:32 -0400

    Remove hardcoded user from launch vnc

--- launchvncserver.sh
+++ launchvncserver.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-tigervncserver -localhost no -geometry 1920x1080  -PAMService=login -PlainUsers=lemniscate -SecurityTypes=TLSPlain
+tigervncserver -localhost no -geometry 1920x1080  -PAMService=login -PlainUsers="$(whoami)" -SecurityTypes=TLSPlain
